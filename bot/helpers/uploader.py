5d635799be7094764b0d936d6bd615e76d0ce0d8
X LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-04-15 20:14:20 +0530

    Update uploader.py

--- bot/helpers/uploader.py
+++ bot/helpers/uploader.py
@@ -69,7 +69,7 @@ class Uploader:
             )
             description = (
                 Config.VIDEO_DESCRIPTION
-                + "\nUploaded to YouTube with https://tx.me/youtubeitbot"
+                + "\n#pw\n#share\n#subscribe\n#LegendBoy\n#legendbot"
             )[:5000]
             if not Config.UPLOAD_MODE:
                 privacyStatus = "private"
